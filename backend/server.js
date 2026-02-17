e539d12b0b05b97f49f7779e2ecf2cd02cc70e93
Onkar-Mane-3333 <maneonkar67@gmail.com>
2026-02-17 15:58:08 +0530

    add render port config

--- backend/server.js
+++ backend/server.js
@@ -261,6 +261,8 @@ app.post("/chat", verifyToken ,async (req,res) => {
 
 app.use(errorHandler);
 
-app.listen(5000, () => {
-  console.log("Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running");
 });
